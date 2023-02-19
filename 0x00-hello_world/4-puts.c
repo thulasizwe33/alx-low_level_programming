57877649cef90b81f3e6f15eff9a6a1aa2b67b1c
Thulane <thulasizwe3@gmail.com>
2023-02-18 23:37:20 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h/
+#include <stdio.h>
 /**
  *
  * main	-Print a line of code using puts
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle)"
-	 return (0);
-}  	 
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
