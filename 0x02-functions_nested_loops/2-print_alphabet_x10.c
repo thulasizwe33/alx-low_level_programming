bebeb1fc6640637f7fc409454c6278f0c3fe42e0
Thulane <thulasizwe3@gmail.com>
2023-02-21 14:35:17 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void);
+{
+	char letter;
+
+	while (count++ <= 9)
+	{
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter;
+		_putchar('\n');
+	}
+
+}
