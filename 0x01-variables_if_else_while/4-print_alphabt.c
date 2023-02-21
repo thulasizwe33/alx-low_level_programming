15a95a6f4386cff87307b8e126806f18db471185
Thulane <thulasizwe3@gmail.com>
2023-02-20 18:56:49 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,9 +15,9 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a'; low <= 'z'; low++)
+	for(low = 'a'; low <= 'z'; low++)
 	{
-	if (low != e && != q)
+	if (low != e && low != q)
 	putchar(low);
 	}
 	putchar('\n');
