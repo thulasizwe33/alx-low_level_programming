58a9c9c267ee708c3af292bdad1cc3e8834020ba
Thulane <thulasizwe3@gmail.com>
2023-02-21 11:39:39 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * main - Entry point
+ * main -print _putchar
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
 
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
