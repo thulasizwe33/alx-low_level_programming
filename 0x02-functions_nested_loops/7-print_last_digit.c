a65a6b6c7ecaa3c2a058ebb7547a9fc2b71a1c27
Thulane <thulasizwe3@gmail.com>
2023-02-22 12:09:15 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,19 @@
 
 /**
  * print_last digit -prints the last digit
- * @n: the passed args
- * Return: the last digit
+ * @n: The number to be treated
+ * Return: Value of the last digit of number
  */
 
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int x;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+		n = -n;
+	x = n % 10;
 
-	_putchar(ld + '0');
-
-	return (0);
+	_putchar(x + '0');
+	return (x);
 
 }
